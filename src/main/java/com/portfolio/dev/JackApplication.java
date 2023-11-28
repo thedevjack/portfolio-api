4b33815bdab6d9f73c446f917d61cc7dd582dd85
thedevjack <jackson1santos@hotmail.com>
2023-11-27 23:53:18 -0300

    Primeiro passo

---
+++ src/main/java/com/portfolio/dev/JackApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/dev/JackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
